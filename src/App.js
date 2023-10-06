f1f95d21e5156df5b190b9254d5e8545ce34087f
Harish Choudhary <harishchoudhary2606@gmail.com>
2023-10-06 09:43:21 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { useEffect } from 'react'
 import './App.css';
 import TopNav from './components/TopNav/TopNav';
-// import Card from './components/Card/Card';
 import DashView from './components/DashBoard/DashView';
 import { useDispatch, useSelector} from 'react-redux'
 import { fetchAllData } from './Actions/DataAction';
-import Loading from './components/Loading/Loading';
 
 const App = () => {
   const dispatch = useDispatch();
@@ -15,13 +13,13 @@ const App = () => {
     dispatch(fetchAllData());
   }, [dispatch])
 
-  return allTickets ? (
+  return (
     <div style={{paddingTop : "10px"}} >
       <TopNav/>
       <hr style={{marginTop : "10px"}} />
       <DashView/>
     </div>
-  ) : <Loading/>
+  )
 }
 
 export default App
